9178684c5f45f997333b8473f265a19cbe4e1dd6
英文名 <>
2025-11-07 09:47:54 +0800

    ㄕ

--- snale.js
+++ snale.js
@@ -44,14 +44,102 @@ apple = {
     x: 5,
     y: 5,
     //畫蘋果
-    drawApple: function () {
+    drawApple: function () {ctx.fillStyle = 'red';
+ctx.fillRect(
+    this.x * BLOCK_SIZE ,
+    this.y * BLOCK_SIZE ,
+    BLOCK_SIZE ,
+    BLOCK_SIZE
+)
     },
     //放蘋果
-    putApple: function () {
+    putApple: function () {this.x = Math.floor(Math.random() * MAP_SIZE);
+this.y = Math.floor(Math.random() * MAP_SIZE);
+
     },
 }
 
 
 
+function gameStart() {
+    gameInterval = setInterval(drawGame, 100);
+}
+/////
+gameStart(); //執行開始遊戲
 
+function keyDown(event) {
+    //up
+    if (event.keyCode == 38 || event.keyCode == 87){ 
+         if (snake.direction.y == 1) return;
+            snake.direction.y = -1;
+            snake.direction.x = 0;
+    }
+    //down
+    else if (event.keyCode == 40 || event.keyCode == 83) {
+        if (snake.direction.y == -1) return;
+            snake.direction.y = 1;
+            snake.direction.x = 0;
+    }
+    //left
+    else if (event.keyCode == 37 || event.keyCode == 65) {
+        if (snake.direction.x == 1) return;
+            snake.direction.x = -1;
+            snake.direction.y = 0;
+    }
+    //right
+    else if (event.keyCode == 39 || event.keyCode == 68) {
+        if (snake.direction.x == -1) return;
+            snake.direction.x = 1;
+            snake.direction.y = 0;
+    }
+}
+/////
+document.addEventListener("keydown", keyDown);
 
+const BLOCK_SIZE = 20;  //放大畫素，20點為一格
+const MAP_SIZE = canvas.width/BLOCK_SIZE ; // (寬400 / 格20) = 20格子(列)
+let score = 0;      // 紀錄分數
+
+function drawGame() {
+    drawMap();
+    apple.drawApple();
+    snake.drawSnake();
+    eatApple(); 
+    drawScore();
+    checkDeath();    
+}
+function drawMap() {
+    ctx.fillStyle = 'black' ;
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function eatApple() {
+    if (snake.body[0].x === apple.x && snake.body[0].y === apple.y) {
+        snake.size += 1;
+        score++;
+        apple.putApple();
+    }
+}
+function drawScore() {
+    ctx.fillStyle = "white";
+    ctx.font = "10px Verdana";
+    ctx.fillText("Score " + score, canvas.width - 50, 10);    
+}
+
+function checkDeath() {
+    // hit walls
+    if( (snake.body[0].x < 0) ||
+        (snake.body[0].x >= MAP_SIZE)||
+        (snake.body[0].y < 0) ||
+        (snake.body[0].y >= MAP_SIZE)
+    ){
+        clearInterval(gameInterval);
+    }
+    // hit body
+    for (var i=1; i<snake.body.length; i++) {
+        if (snake.body[0].x === snake.body[i].x &&
+            snake.body[0].y === snake.body[i].y) {
+                clearInterval(gameInterval);
+            }  
+    }
+}
